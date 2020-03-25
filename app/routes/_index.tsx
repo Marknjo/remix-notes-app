66a496279c2e3ab752636cb14c67a60f4623eda8
Mark Njoroge <marknjo7@gmail.com>
2020-03-25 08:59:37 +0300

    add home styles to the project

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
-import type { V2_MetaFunction } from '@remix-run/node'
+import type { LinksFunction, V2_MetaFunction } from '@remix-run/node'
 import { Link } from '@remix-run/react'
+import homeStyles from '~/styles/home.css'
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -11,11 +12,20 @@ export const meta: V2_MetaFunction = () => {
 export default function Index() {
   return (
     <main id="content">
-      <h1>A better way of keeping track of your notes</h1>
-      <p>Try our early beta and never loose track of your notes again!</p>
-      <p id="cta">
-        <Link to="/notes">Try Now!</Link>
-      </p>
+      <section>
+        <h1>A better way of keeping track of your notes</h1>
+        <p>Try our early beta and never loose track of your notes again!</p>
+        <p id="cta">
+          <Link to="/notes">Try Now!</Link>
+        </p>
+      </section>
     </main>
   )
 }
+
+export const links: LinksFunction = () => [
+  {
+    rel: 'stylesheet',
+    href: homeStyles,
+  },
+]
