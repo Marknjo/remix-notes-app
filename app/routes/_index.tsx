945f83265c271f159d6db1cb22e4c0c1bc6a03ba
Mark Njoroge <marknjo7@gmail.com>
2020-03-27 10:18:43 +0300

    fix positioning of the index page content

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -12,13 +12,11 @@ export const meta: V2_MetaFunction = () => {
 export default function Index() {
   return (
     <main id="content">
-      <section>
-        <h1>A better way of keeping track of your notes</h1>
-        <p>Try our early beta and never loose track of your notes again!</p>
-        <p id="cta">
-          <Link to="/notes">Try Now!</Link>
-        </p>
-      </section>
+      <h1>A better way of keeping track of your notes</h1>
+      <p>Try our early beta and never loose track of your notes again!</p>
+      <p id="cta">
+        <Link to="/notes">Try Now!</Link>
+      </p>
     </main>
   )
 }
