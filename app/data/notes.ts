038bd218c230082bcebe200c7c35cc0ae8e4a6b2
Mark Njoroge <marknjo7@gmail.com>
2020-03-30 10:34:51 +0300

    add getStoreNotes() & createNotes methods

---
+++ app/data/notes.ts
@@ -0,0 +1,28 @@
+import { json } from '@remix-run/node'
+import fs from 'fs/promises'
+
+export interface INote {
+  title: string
+  body: string
+}
+
+export async function getStoredNotes(): Promise<Array<INote> | []> {
+  try {
+    const rawFileContent = await fs.readFile('notes.json', {
+      encoding: 'utf-8',
+    })
+    const data = JSON.parse(rawFileContent)
+
+    const storedNotes = data.notes ?? []
+    return storedNotes
+  } catch (error) {
+    throw json({
+      message: 'Could not load notes from the store',
+      statusCode: 500,
+    })
+  }
+}
+
+export function createNotes(notes: INote) {
+  return fs.writeFile('notes.json', JSON.stringify({ notes: notes || [] }))
+}
