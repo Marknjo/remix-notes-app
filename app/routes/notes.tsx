0d00e1953190a7332ed933a95f96d54863a82660
Mark Njoroge <marknjo7@gmail.com>
2020-04-04 11:50:47 +0300

    load notes from the store

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,21 +1,34 @@
-import {
-  redirect,
-  type ActionFunction,
-  type LinksFunction,
+import { redirect } from '@remix-run/node'
+import type {
+  LoaderFunction,
+  ActionFunction,
+  LinksFunction,
 } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
 import NewNote, { links as newNoteStyles } from '~/components/NewNote'
+import NoteList, { links as noteListStyles } from '~/components/NoteList'
 import type { INote } from '~/data/notes'
-import { createNote } from '~/data/notes'
+import { createNote, getStoredNotes } from '~/data/notes'
+
+export const loader: LoaderFunction = async () => {
+  return getStoredNotes()
+}
 
 export default function NotesPage() {
+  const notes = useLoaderData<Array<INote> | []>()
+
   return (
     <main>
       <NewNote />
+      <NoteList notes={notes} />
     </main>
   )
 }
 
-export const links: LinksFunction = () => [...newNoteStyles()]
+export const links: LinksFunction = () => [
+  ...newNoteStyles(),
+  ...noteListStyles(),
+]
 
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData()
