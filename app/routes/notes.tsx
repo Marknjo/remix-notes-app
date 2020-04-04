5ae7e2cfb3aecf3ac37190dde87722e359bbd807
Mark Njoroge <marknjo7@gmail.com>
2020-04-04 11:33:36 +0300

    implement user create a new note

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,5 +1,11 @@
-import type { LinksFunction } from '@remix-run/node'
+import {
+  redirect,
+  type ActionFunction,
+  type LinksFunction,
+} from '@remix-run/node'
 import NewNote, { links as newNoteStyles } from '~/components/NewNote'
+import type { INote } from '~/data/notes'
+import { createNote } from '~/data/notes'
 
 export default function NotesPage() {
   return (
@@ -10,3 +16,15 @@ export default function NotesPage() {
 }
 
 export const links: LinksFunction = () => [...newNoteStyles()]
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData()
+  const newNote: Pick<INote, 'description' | 'title'> = Object.fromEntries(
+    formData,
+  ) as Pick<INote, 'description' | 'title'>
+
+  // validation
+  await createNote(newNote)
+
+  return redirect('.')
+}
